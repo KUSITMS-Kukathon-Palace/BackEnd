6992bd87c4f73489beb19b16f303e649569ae48b
seeunjang <wisjang1@naver.com>
2023-03-25 14:37:17 +0900

    [feat] 초기 세팅

---
+++ src/main/java/com/palace/backend/PalaceApplication.java
@@ -0,0 +1,13 @@
+package com.palace.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/palace/backend/PalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palace.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
