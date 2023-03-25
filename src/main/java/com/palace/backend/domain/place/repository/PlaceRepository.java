2a22e88dd2a926e05caf9dee23468c13ce160c35
hojeong2747 <hojeong2747@gmail.com>
2023-03-26 03:12:22 +0900

    [feat] 상세 페이지, 장소 id

--- src/main/java/com/palace/backend/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/palace/backend/domain/place/repository/PlaceRepository.java
@@ -3,6 +3,7 @@ package com.palace.backend.domain.place.repository;
 import com.palace.backend.domain.place.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,4 +14,10 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
 
     @Query("select p from Place p where p.name in :place")
     Place findPlaceIn(String place);
+
+    @Query(value = "select * from place p where p.name = :name", nativeQuery = true)
+    List<Place> findPlaceDetail(@Param("name") String name);
+
+    @Query(value = "select p.place_id from place p where p.name = :name", nativeQuery = true)
+    Long findPlaceId(@Param("name") String name);
 }
